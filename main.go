ac7761cb911e316b5e36a1324be9afb611fbd651
watermelon1024 <st102060@chps.tp.edu.tw>
2023-08-12 17:19:07 +0800

    feat: backend shutdown hint

--- main.go
+++ main.go
@@ -89,12 +89,12 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
-	log.Println("Shutdown Server ...")
+	log.Println("Server shutting down...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
+		log.Fatal("Shutdown Error: ", err)
 	}
-	log.Println("Server exiting")
+	log.Println("Server has been shutdown.")
 }
