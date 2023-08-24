3c11dd66668715af2693bd927dc494dafd01fe58
watermelon1024 <st102060@chps.tp.edu.tw>
2023-08-24 20:35:52 +0800

    feat: add rate limited

--- go.mod
+++ go.mod
@@ -5,15 +5,17 @@ go 1.20
 require (
 	github.com/compose-spec/compose-go v1.18.2
 	github.com/gin-gonic/gin v1.9.1
-	go.uber.org/ratelimit v0.3.0
+	github.com/redis/go-redis/v9 v9.0.4
+	github.com/ulule/limiter/v3 v3.11.2
 	golang.org/x/net v0.14.0
 )
 
 require (
-	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/bytedance/sonic v1.10.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- utils/rate.go
+++ utils/rate.go
@@ -3,23 +3,28 @@ package utils
 import (
 	"time"
 
-	"github.com/gin-gonic/gin"
-	"go.uber.org/ratelimit"
+	"github.com/redis/go-redis/v9"
+
+	limiter "github.com/ulule/limiter/v3"
+	mgin "github.com/ulule/limiter/v3/drivers/middleware/gin"
+	sredis "github.com/ulule/limiter/v3/drivers/store/redis"
 )
 
-const RPS = 100
+var (
+	Middleware gin.HandlerFunc
+)
 
-var limit = ratelimit.New(RPS)
-
-func LeakBucket() gin.HandlerFunc {
-	prev := time.Now()
-
-	return func(c *gin.Context) {
-		now := limit.Take()
-		if now.Sub(prev) < 10*time.Second {
-			c.JSON(429, gin.H{"error": "too many requests"})
-			c.Abort()
-		}
-		prev = now
+func init() {
+	rate := limiter.Rate{
+		Period: 10 * time.Minute,
+		Limit:  50,
 	}
+	option, _ := redis.ParseURL("redis://localhost:6379/0")
+	client := redis.NewClient(option)
+
+	store, _ := sredis.NewStoreWithOptions(client, limiter.StoreOptions{
+		Prefix:   "limiter_prefix",
+	})
+
+	Middleware = mgin.NewMiddleware(limiter.New(store, rate))
 }
