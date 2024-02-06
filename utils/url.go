eacd7eea2d8818e2e8adc043dca65c77ccce959a
watermelon1024 <watermelonman1024@gmail.com>
2024-02-06 19:22:23 +0800

    refa: split files
    split db into a separate file

--- utils/url.go
+++ utils/url.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"path/filepath"
 	"regexp"
 
 	"github.com/compose-spec/compose-go/dotenv"
@@ -29,9 +28,6 @@ type (
 var (
 	SHORT_LEN = len(SHORT_KEYS)
 
-	// sqlite database
-	db *sql.DB
-
 	// URL validation regex
 	reURL       = regexp.MustCompile(`^(https?://)([\S]+\.)?([^\s/]+\.[^\s/]{2,})(/?[\S]+)?$`)
 	reCustomURL = regexp.MustCompile(`^([\w\-]{1,32})$`)
@@ -43,50 +39,6 @@ var (
 func init() {
 	dotenv.Load()
 	HOSTNAME = os.Getenv("HOSTNAME")
-	dbFilePath := os.Getenv("DB_PATH")
-
-	var err error
-	// check/create database dir
-	dir := filepath.Dir(dbFilePath)
-	if _, err = os.Stat(dir); os.IsNotExist(err) {
-		// dir not exist, create it
-		err := os.MkdirAll(dir, 0755)
-		if err != nil {
-			log.Fatalln("Error creating directory:", dir)
-		}
-	}
-	// check/create database file
-	if _, err = os.Stat(dbFilePath); os.IsNotExist(err) {
-		// file not exist, create it
-		file, err := os.Create(dbFilePath)
-		if err != nil {
-			log.Fatalln("Error creating database file:", err)
-		}
-		file.Close()
-	}
-	// connect to database
-	db, err = sql.Open("sqlite3", dbFilePath)
-	if err != nil {
-		log.Fatalln("Error opening database:", err)
-	}
-	// create tables
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS urls (
-		id TEXT PRIMARY KEY,
-		target_url TEXT NOT NULL,
-		meta TEXT,
-		count INTEGER DEFAULT 0,
-		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-		created_by TEXT,
-		ip TEXT,
-		expired_at DATETIME
-	)`)
-	if err != nil {
-		log.Fatalln("Error creating table:", err)
-	}
-}
-
-func CloseDB() error {
-	return db.Close()
 }
 
 // Custom Meta Data
